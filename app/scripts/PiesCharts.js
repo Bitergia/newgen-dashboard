d153db356f9525ece35bd8bea13b972353e41c5d
Luis Cañas Díaz <lcanas@bitergia.com>
2015-07-18 19:20:12 -0700

    Code refactoring for PieCharts.js

--- app/scripts/PiesCharts.js
+++ app/scripts/PiesCharts.js
@@ -1,17 +1,31 @@
-function Pies(){
+/*
+ *
+ */
 
-    compPie = dc.pieChart('#compPieChart');
-    commitsNamePie = dc.pieChart('#authPieChart');
-    repoPie = dc.pieChart('#repoPieChart');
+function Pies(){
+    drawPie('#compPieChart','#filterComp','company');
+    drawPie('#authPieChart','#filterDeve','name');
+    drawPie('#repoPieChart','#filterRepo','repo');
+}
+
+/*
+ * Draws the actual pie using
+ * @param {string} pie_id - HTML tag
+ * @param {string} filter_id - HTML tag
+ * @param {string} key - key used to get the dimension
+ *
+ */
+function drawPie(pie_id, filter_id, key){
+    pie = dc.piechart(pie_id);
 
     var dim = ndx.dimension(function(d){
-        return d.company;
-    })
+        return d[key];
+    });
 
-    var grp = dim.group()
+    var grp = dim.group();
 
-    compPie
-    .width(10000)
+    pie
+    .width(350)
     .height(250)
     .dimension(dim)
     .group(grp)
@@ -20,144 +34,52 @@ function Pies(){
     .legend(dc.legend().x(0).y(3).itemHeight(13).gap(5))
     .ordering(function (d) { return -d.value; });
 
-	compPie.on('renderlet', function(chart) {
-	  chart.selectAll('.pie-slice').on("click", function(d) {
-    	document.dispatchEvent(pieClickEvent);
-	  });
-	});
-    compPie.on("filtered", function(chart,filter) {
-        if(filter==null){
-            compFilters=[]
-            $("#filterComp").empty()
+    pie.on('renderlet', function(chart) {
+        chart.selectAll('.pie-slice').on("click", function(d) {
+            document.dispatchEvent(pieClickEvent);
+        });
+    });
+
+    pie.on("filtered", function(chart,filter) {
+        if(filter === null){
+            compFilters=[];
+            $(filter_id).empty();
         }else if(filter!="Others"){
             if(filter.constructor==Array){
                 filter[0].forEach(function(element){
                     if(compFilters.indexOf(element)==-1){
-                        $("#filterComp").append('<span id="filter-'+element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+element+' </span>')
-                        compFilters.push(element)
+                        $(filter_id).append('<span id="filter-' + formatElement(element) + '"> '+element+' </span>');
+                        compFilters.push(element);
                     }else{
-                        $('#filter-'+(element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                        compFilters.splice(compFilters.indexOf(element),1)
+                        $('#filter-' + (formatElement(element))).remove();
+                        compFilters.splice(compFilters.indexOf(element),1);
                     }
-                })
+                });
             }else{
                 if(compFilters.indexOf(filter)==-1){
-                    $("#filterComp").append('<span id="filter-'+filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+filter+' </span>')
-                    compFilters.push(filter)
+                    $(filter_id).append('<span id="filter-' + formatElement(filter) + '"> ' + filter + ' </span>');
+                    compFilters.push(filter);
                 }else{
-                    $('#filter-'+(filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                    compFilters.splice(compFilters.indexOf(filter),1)
+                    $('#filter-' + (formatElement(filter))).remove();
+                    compFilters.splice(compFilters.indexOf(filter),1);
                 }
             }
         }
         window.history.replaceState("object or string", "Title", writeURL());
-
     });
+}
 
-
-    var dim2 = ndx.dimension(function(d){
-        return d.name;
-    })
-
-    dim2.group().all().forEach(function(element){
-        users.push(element.key)
-    })
-
-    var grp2 = dim2.group()
-
-    commitsNamePie
-    .width(350)
-    .height(250)
-    .dimension(dim2)
-    .group(grp2)
-    .cx(225)
-    .cap(10)
-    .legend(dc.legend().x(0).y(3).itemHeight(13).gap(5))
-    .ordering(function (d) { return -d.value; });
-
-	commitsNamePie.on('renderlet', function(chart) {
-	  chart.selectAll('.pie-slice').on("click", function(d) {
-		document.dispatchEvent(pieClickEvent);
-	  });
-	});
-    commitsNamePie.on("filtered", function(chart,filter) {
-        if(filter==null){
-            repoFilters=[]
-            $("#filterDeve").empty()
-        }else if(filter!="Others"){
-            if(filter.constructor==Array){
-                filter[0].forEach(function(element){
-                    if(repoFilters.indexOf(element)==-1){
-                        $("#filterDeve").append('<span id="filter-'+element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+element+' </span>')
-                        repoFilters.push(element)
-                    }else{
-                        $('#filter-'+(element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                        repoFilters.splice(repoFilters.indexOf(element),1)
-                    }
-                })
-            }else{
-                if(repoFilters.indexOf(filter)==-1){
-                    $("#filterDeve").append('<span id="filter-'+filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+filter+' </span>')
-                    repoFilters.push(filter)
-                }else{
-                    $('#filter-'+(filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                    repoFilters.splice(repoFilters.indexOf(filter),1)
-                }
-            }
-        }
-        window.history.replaceState("object or string", "Title", writeURL());
-
-    });
-
-    var dim3 = ndx.dimension(function(d){
-        return d.repo;
-    })
-
-    var grp3 = dim3.group()
-
-    repoPie
-    .width(350)
-    .height(250)
-    .dimension(dim3)
-    .group(grp3)
-    .cx(225)
-    .cap(10)
-    .legend(dc.legend().x(0).y(3).itemHeight(13).gap(5))
-    .ordering(function (d) { return -d.value; });
-
-	repoPie.on('renderlet', function(chart) {
-	  chart.selectAll('.pie-slice').on("click", function(d) {
-		document.dispatchEvent(pieClickEvent);
-	  });
-	});
-    repoPie.on("filtered", function(chart,filter) {
-        if(filter==null){
-            repoFilters=[]
-            $("#filterRepo").empty()
-        }else if(filter!="Others"){
-            if(filter.constructor==Array){
-                filter[0].forEach(function(element){
-                    if(repoFilters.indexOf(element)==-1){
-                        $("#filterRepo").append('<span id="filter-'+element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+element+' </span>')
-                        repoFilters.push(element)
-                    }else{
-                        $('#filter-'+(element.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                        repoFilters.splice(repoFilters.indexOf(element),1)
-                    }
-                })
-            }else{
-                if(repoFilters.indexOf(filter)==-1){
-                    $("#filterRepo").append('<span id="filter-'+filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0")+'"> '+filter+' </span>')
-                    repoFilters.push(filter)
-                }else{
-                    $('#filter-'+(filter.replaceAll(" ","0").replaceAll(".","0").replaceAll(",","0").replaceAll("(","0").replaceAll(")","0").replaceAll("?","0").replaceAll("'","0").replaceAll("@","0"))).remove()
-                    repoFilters.splice(repoFilters.indexOf(filter),1)
-                }
-            }
-        }
-        window.history.replaceState("object or string", "Title", writeURL());
-
-    });
-
-
+/*
+ * Returns string with some characters replaced by 0
+ * @param {string} mystring - input string
+ */
+function formatElement(mystring){
+    return mystring.replaceAll(" ","0").
+                    replaceAll(".","0").
+                    replaceAll(",","0").
+                    replaceAll("(","0").
+                    replaceAll(")","0").
+                    replaceAll("?","0").
+                    replaceAll("'","0").
+                    replaceAll("@","0");
 }
